6c549fbac5e0e239d886d13c439a52d2dad18f3e
epaillas <epaillas@astro.puc.cl>
2020-04-01 22:10:15 -0300

    update

--- python_tools/split_densities.py
+++ python_tools/split_densities.py
@@ -6,25 +6,25 @@ import matplotlib.pyplot as plt
 
 # read file name from command line
 @click.command()
-@click.option('--gal_den_file', type=str, required=True)
-@click.option('--dm_den_file', type=str, required=True)
+@click.option('--gal_den_monopole', type=str, required=True)
+@click.option('--dm_den_monopole', type=str, required=True)
 @click.option('--has_velocity', type=bool, required=True)
 @click.option('--handle', type=str, required=True)
 
-def split_densities(gal_den_file,
-                         dm_den_file,
-                         has_velocity,
-                         handle):
+def split_densities(gal_den_monopole,
+                    dm_den_monopole,
+                    has_velocity,
+                    handle):
 
 
     print('\nSplitting densities for the following arguments:')
-    print('gal_den_file: {}'.format(gal_den_file))
-    print('dm_den_file: {}'.format(dm_den_file))
+    print('gal_den_monopole: {}'.format(gal_den_monopole))
+    print('dm_den_monopole: {}'.format(dm_den_monopole))
     print('has_velocity: {}'.format(has_velocity))
     print('handle: {}'.format(handle))
 
     # open galaxy density file and get dimensions
-    f = FortranFile(gal_den_file, 'r')
+    f = FortranFile(gal_den_monopole, 'r')
     ncentres = f.read_ints()[0]
     nbins = f.read_ints()[0]
     print('ncentres, nbins = ({}, {})'.format(ncentres, nbins))
@@ -32,8 +32,8 @@ def split_densities(gal_den_file,
     # read raw data and close file
     rbin = f.read_reals()
     gal_dd = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
-    gal_delta = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
-    gal_cumdelta = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
+    xi_r = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
+    xibar_r = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
 
 
     if has_velocity:
@@ -43,15 +43,15 @@ def split_densities(gal_den_file,
     f.close()
 
     # open DM density file
-    f = FortranFile(dm_den_file, 'r')
+    f = FortranFile(dm_den_monopole, 'r')
     ncentres = f.read_ints()[0]
     nbins = f.read_ints()[0]
     print('ncentres, nbins = ({}, {})'.format(ncentres, nbins))
 
     rbin = f.read_reals()
     dm_dd = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
-    dm_delta = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
-    dm_cumdelta = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
+    delta_r = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
+    Delta_r = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
 
     if has_velocity:
         dm_vr = f.read_reals(dtype=np.float64).reshape(nbins, ncentres)
@@ -62,12 +62,12 @@ def split_densities(gal_den_file,
 
     # assemble individual profiles into an array
     if has_velocity:
-        profiles = [np.c_[gal_dd[:,i], gal_delta[:,i], gal_cumdelta[:,i],
-                    dm_delta[:,i], dm_cumdelta[:,i],
+        profiles = [np.c_[gal_dd[:,i], xi_r[:,i], xibar_r[:,i],
+                    delta_r[:,i], Delta_r[:,i],
                     gal_vr[:,i], gal_sv_los[:,i]] for i in range(ncentres)]
     else:
-        profiles = [np.c_[gal_dd[:,i], gal_delta[:,i], gal_cumdelta[:,i],
-                    dm_cumdelta[:,i]]
+        profiles = [np.c_[gal_dd[:,i], xi_r[:,i], xibar_r[:,i],
+                    Delta_r[:,i]]
                     for i in range(ncentres)]
 
     profiles = np.asarray(profiles)
@@ -81,17 +81,17 @@ def split_densities(gal_den_file,
         profiles['den{}'.format(i)] = sorted_profiles[int((i-1)*ncentres/5):int(i*ncentres/5)]
 
     # get average densities
-    gal_delta = {}
-    dm_delta = {}
-    gal_cumdelta = {}
-    dm_cumdelta = {}
+    xi_r = {}
+    delta_r = {}
+    xibar_r = {}
+    Delta_r = {}
 
     for i in range(1, 6):
         den = profiles['den{}'.format(i)]
-        gal_delta['den{}'.format(i)] = np.mean(den, axis=0)[:,-6]
-        gal_cumdelta['den{}'.format(i)] = np.mean(den, axis=0)[:,-5]
-        dm_delta['den{}'.format(i)] = np.mean(den, axis=0)[:,-4]
-        dm_cumdelta['den{}'.format(i)] = np.mean(den, axis=0)[:,-3]
+        xi_r['den{}'.format(i)] = np.mean(den, axis=0)[:,-6]
+        xibar_r['den{}'.format(i)] = np.mean(den, axis=0)[:,-5]
+        delta_r['den{}'.format(i)] = np.mean(den, axis=0)[:,-4]
+        Delta_r['den{}'.format(i)] = np.mean(den, axis=0)[:,-3]
 
     # get average vr
     gal_vr = {}
@@ -113,15 +113,15 @@ def split_densities(gal_den_file,
 
 
     for i in range(1,6):
-        dm_delta_file = handle + '_den{}'.format(i) + '.CCF_DM_monopole'
-        gal_delta_file = handle + '_den{}'.format(i) + '.CCF_gal_monopole'
+        delta_r_file = handle + '_den{}'.format(i) + '.CCF_dm_den_monopole'
+        xi_r_file = handle + '_den{}'.format(i) + '.CCF_gal_den_monopole'
         gal_vr_file = handle + '_den{}'.format(i) + '.CCF_gal_vr'
         gal_sv_los_file = handle + '_den{}'.format(i) + '.CCF_gal_sv_los'
 
         fmt = 2*'%10.3f '
         
-        np.savetxt(dm_delta_file, np.c_[rbin, dm_delta['den{}'.format(i)]], fmt=fmt)
-        np.savetxt(gal_delta_file, np.c_[rbin, gal_delta['den{}'.format(i)]], fmt=fmt)
+        np.savetxt(delta_r_file, np.c_[rbin, delta_r['den{}'.format(i)]], fmt=fmt)
+        np.savetxt(xi_r_file, np.c_[rbin, xi_r['den{}'.format(i)]], fmt=fmt)
         np.savetxt(gal_vr_file, np.c_[rbin, gal_vr['den{}'.format(i)]], fmt=fmt)
         np.savetxt(gal_sv_los_file, np.c_[rbin, gal_sv_los['den{}'.format(i)]], fmt=fmt)
 
