a37273e8d75b95aea89745a85c56914410e259b0
epaillas <epaillas@astro.puc.cl>
2020-04-22 19:45:22 -0400

    update

--- python_tools/jointfitcovariance.py
+++ python_tools/jointfitcovariance.py
@@ -132,7 +132,7 @@ def MultipoleCovariance(handle_mocks, smins, smaxs, full_fit=True):
 
 
         mock_datavec = np.asarray(mock_datavec)
-        cov = getCovarianceMatrix(mock_datavec)o
+        cov = getCovarianceMatrix(mock_datavec)
         return cov
 
 @click.command()
