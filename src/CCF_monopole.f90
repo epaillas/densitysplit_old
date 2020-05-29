5c867963381f4dbba1179066a8f02873655d84aa
epaillas <epaillas@astro.puc.cl>
2020-05-28 21:08:16 -0400

    update

--- src/CCF_monopole.f90
+++ src/CCF_monopole.f90
@@ -270,9 +270,9 @@ program density_profiles
   open(12, file=output_den, status='replace')
   do i = 1, nrbin
     if (has_velocity) then
-      write(12, fmt='(4f15.5)') rbin(i), delta(i), int_delta(i), mean_vr(i), std_vlos(i)
+      write(12, fmt='(5f15.5)') rbin(i), delta(i), int_delta(i), mean_vr(i), std_vlos(i)
     else
-      write(12, fmt='(2f15.5)') rbin(i), delta(i), int_delta(i)
+      write(12, fmt='(3f15.5)') rbin(i), delta(i), int_delta(i)
     end if
   end do
 
